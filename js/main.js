e7d0de37d381d72fc702b64ce591c09dc955e539
Afief Sholichuddin <apiep.oedin@gmail.com>
2019-02-26 15:36:47 +0700

    Fix slow method call when submitting form

--- js/main.js
+++ js/main.js
@@ -17,39 +17,4 @@ requirejs.config({
   }
 })
 
-requirejs([
-  'jquery', 'service/qiscus',
-  'service/route', 'service/emitter',
-  'service/content',
-  'pages/login', 'pages/chat-list', 'pages/chat',
-  'pages/users'
-], function (
-  $, qiscus,
-  route, emitter,
-  $content,
-  LoginPage, ChatList, Chat,
-  Users
-) {
-  var currentPage = route.current || '/login'
-  var routes = [LoginPage, ChatList, Chat, Users]
-
-  // console.log('$', $)
-  window.$jquery = $
-  window.qiscus = qiscus
-
-  if (!qiscus.isLogin) {
-    $content.html(LoginPage())
-  } else {
-    $content.html(ChatList())
-  }
-
-  emitter.on('qiscus::login-success', function (authData) {
-    $content.html(ChatList)
-  })
-  emitter.on('route::change', function (location) {
-    var routecontent = routes.find(function (route) {
-      return route.path === location.pathname
-    })
-    $content.html(routecontent(location.state))
-  })
-})
+requirejs(['app'])
--- js/pages/login.js
+++ js/pages/login.js
@@ -1,38 +1,46 @@
 define([
-  'jquery', 'service/route', 'service/qiscus'
-], function ($, route, qiscus) {
+  'jquery', 'service/route', 'service/content', 'service/qiscus'
+], function ($, route, $content, qiscus) {
 
-  function LoginPage() {
-    $('#qiscus-widget').on('submit', '#LoginForm', function (event) {
-      event.preventDefault()
-      // route.push('/chat')
-      var email = event.target['email']
-      var displayName = event.target['displayname']
-      qiscus.setUser(email, displayName)
-        .then(function (data) {
-          route.push('/chat')
-        })
+  function doLogin(userId, userKey, displayName) {
+    qiscus.setUser(userId, userKey, displayName)
+  }
+  function LoginPage(state) {
+    // For some reason, jquery.on('submit') are very slow
+    // and did not want to call qiscus.setUser
+    document.addEventListener('submit', function (event) {
+      if (event.target.id === 'LoginForm') {
+        event.preventDefault()
+        var userId = $('#user-id').val()
+        var userKey = $('#user-key').val()
+        var displayName = $('#displayname').val()
+        doLogin(userId, userKey, displayName)
+      }
     })
     return `
       <div class="LoginPage">
         <img src="/img/logo.svg" class="logo">
         <form id="LoginForm">
           <div class="form-group">
-            <label for="email">Email</label>
-            <input id="email" type="email" name="email">
+            <label for="userId">User ID</label>
+            <input id="user-id" type="text" name="user-id" value="afief">
+          </div>
+          <div class="form-group">
+            <label for="user-key">User Key</label>
+            <input id="user-key" type="password" name="user-key" value="qwe12345">
           </div>
           <div class="form-group">
             <label for="displayname">Display Name</label>
-            <input id="displayname" type="text" name="displayname">
+            <input id="displayname" type="text" name="displayname" value="afief-test-1">
           </div>
           <div class="form-group">
-            <button type="submit">Start <i class="fas fa-arrow-right"></i></button>
+            <button type="submit" id="submit-login-btn">Start <i class="fas fa-arrow-right"></i></button>
           </div>
         </form>
       </div>
     `
   }
+  LoginPage.path = '/login'
 
   return LoginPage
-
 })
