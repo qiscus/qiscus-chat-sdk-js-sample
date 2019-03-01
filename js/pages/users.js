e0e7e4f33532e82768a4be28c4ae72ce7d6678b7
Afief Sholichuddin <apiep.oedin@gmail.com>
2019-03-01 13:51:03 +0700

    Fix not working load-more

--- js/pages/users.js
+++ js/pages/users.js
@@ -18,6 +18,84 @@ define([
   var isAbleToLoadMore = true
   var totalUser = 0
 
+
+
+
+  var loadUser = _.debounce(function loadUser(currentLength) {
+    if (isLoadingUser) return
+    if (!isAbleToLoadMore) return
+
+    var perPage = 20
+    var currentPage = Math.ceil(currentLength / perPage)
+    var nextPage = currentPage + 1
+    console.log('nextPage', currentPage, nextPage, currentLength);
+
+    isLoadingUser = true
+    return qiscus.getUsers(searchQuery, nextPage)
+      .then(function (resp) {
+        isLoadingUser = false
+        var users = resp.users.map(contactFormatter).join('')
+        $(users).insertBefore('.contact-list .load-more')
+
+        if (resp.meta.total_page === nextPage) {
+          isAbleToLoadMore = false
+          $content.find('.load-more').css({
+            display: 'none'
+          })
+        }
+      })
+
+  }, 100)
+
+  function handleScroll(event) {
+    var py = event.currentTarget
+    var sy = $(py).find('.scrollspy').get(0)
+
+    var offset = (sy.offsetTop - (py.offsetHeight + sy.scrollHeight + sy.offsetHeight)) - 10
+    var scrollTop = Math.round(py.scrollTop)
+    var shouldLoadMore = scrollTop > offset
+    var childLength = py.children.length - 1
+    if (shouldLoadMore) {
+      loadUser(childLength)
+    }
+  }
+
+  function Users() {
+    qiscus.getUsers()
+      .then(function (resp) {
+        var users = resp.users.map(contactFormatter)
+          .join('')
+        $(users).insertBefore('.contact-list .load-more')
+      })
+    return `
+      <div class="Users">
+        <div class="toolbar">
+          <button type="button" class="back-btn">
+            <i class="icon icon-arrow-back"></i>
+          </button>
+          <div class="toolbar-title">Choose Contacts</div>
+        </div>
+        <button id="create-group-btn" class="create-group-btn">
+          <i class="icon icon-new-chat-group"></i> Create Group Chat
+        </button>
+        <div class="search-container">
+          <i class="icon icon-search"></i>
+          <input type="text" id="search-input" name="search-input" placeholder="Search">
+        </div>
+        <div class="contact-list-container">
+          <div class="contact-list-title">Contacts</div>
+          <ul class="contact-list">
+            <li class="load-more">
+              <button type="button">
+                Load more
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    `
+  }
+
   $content
     .on('click', '.Users .back-btn', function (event) {
       event.preventDefault()
@@ -48,85 +126,19 @@ define([
           $content.find('.contact-list')
             .empty()
             .append(users)
-            .append('<li class="scrollspy">Loading ...</li>')
+            .append('<li class="load-more"><button>Load more</button></li>')
         })
     }, 300))
     .on('click', '.Users .create-group-btn', function (event) {
       event.preventDefault()
       route.push('/create-group')
     })
-
-
-  var loadUser = _.debounce(function loadUser(currentLength) {
-    if (isLoadingUser) return
-    if (!isAbleToLoadMore) return
-
-    var perPage = 20
-    var currentPage = Math.ceil(currentLength / perPage)
-    var nextPage = currentPage + 1
-
-    isLoadingUser = true
-    return qiscus.getUsers(searchQuery, nextPage)
-      .then(function (resp) {
-        isLoadingUser = false
-        var users = resp.users.map(contactFormatter).join('')
-        $(users).insertBefore('.contact-list .scrollspy')
-
-        if (resp.meta.total_page === nextPage) {
-          isAbleToLoadMore = false
-          $content.find('.scrollspy').css({
-            display: 'none'
-          })
-        }
-      })
-
-  }, 100)
-
-  function handleScroll(event) {
-    var py = event.currentTarget
-    var sy = $(py).find('.scrollspy').get(0)
-
-    var offset = (sy.offsetTop - (py.offsetHeight + sy.scrollHeight + sy.offsetHeight)) - 10
-    var scrollTop = Math.round(py.scrollTop)
-    var shouldLoadMore = scrollTop > offset
-    var childLength = py.children.length - 1
-    if (shouldLoadMore) {
+    .on('click', '.Users .load-more button', function (event) {
+      event.preventDefault()
+      var childLength = $content.find('.contact-list').children().length - 1
+      console.log('load-more');
       loadUser(childLength)
-    }
-  }
-
-  function Users() {
-    qiscus.getUsers()
-      .then(function (resp) {
-        var users = resp.users.map(contactFormatter)
-          .join('')
-        $(users).insertBefore('.contact-list .scrollspy')
-        $content.find('.contact-list').on('scroll', handleScroll)
-      })
-    return `
-      <div class="Users">
-        <div class="toolbar">
-          <button type="button" class="back-btn">
-            <i class="icon icon-arrow-back"></i>
-          </button>
-          <div class="toolbar-title">Choose Contacts</div>
-        </div>
-        <button id="create-group-btn" class="create-group-btn">
-          <i class="icon icon-new-chat-group"></i> Create Group Chat
-        </button>
-        <div class="search-container">
-          <i class="icon icon-search"></i>
-          <input type="text" id="search-input" name="search-input" placeholder="Search">
-        </div>
-        <div class="contact-list-container">
-          <div class="contact-list-title">Contacts</div>
-          <ul class="contact-list">
-            <li class="scrollspy">Loading ...</li>
-          </ul>
-        </div>
-      </div>
-    `
-  }
+    })
 
   Users.path = '/users'
   return Users
