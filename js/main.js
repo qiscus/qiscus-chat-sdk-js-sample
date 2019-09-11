5f625dbf933139167917689245eadadd82803c80
Afief Sholichuddin <apiep.oedin@gmail.com>
2019-09-11 11:09:05 +0700

    Fix not updating profile when refreshed

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 requirejs.config({
-  baseUrl: 'js',
-  paths: {
-    jquery: 'lib/jquery-3.3.1.min',
-    dateFns: 'lib/date_fns',
-    history: 'lib/history.min',
-    lodash: 'lib/lodash.min',
-    vhtml: 'lib/vhtml.min',
-    htm: 'lib/htm.umd'
-  },
-  map: {
-    '*': {
-      jquery: 'jquery-mod'
-    },
-    'jquery-mod': {
-      jquery: 'jquery'
-    }
-  }
-})
+	baseUrl: 'js',
+	paths: {
+		jquery: 'lib/jquery-3.3.1.min',
+		dateFns: 'lib/date_fns',
+		history: 'lib/history.min',
+		lodash: 'lib/lodash.min',
+		vhtml: 'lib/vhtml.min',
+		htm: 'lib/htm.umd'
+	},
+	map: {
+		'*': {
+			jquery: 'jquery-mod'
+		},
+		'jquery-mod': {
+			jquery: 'jquery'
+		}
+	}
+});
 
-requirejs(['app'])
+requirejs(['app']);
