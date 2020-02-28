4fb7e525cde8b45e844e4bab1f01b1fda63f7802
yoga-nugroho129 <yoga.nugroho129@gmail.com>
2020-02-28 15:39:38 +0700

    deprecate search container for back-end improvement

--- js/pages/users.js
+++ js/pages/users.js
@@ -1,8 +1,10 @@
 define([
-  'jquery', 'lodash',
+  'jquery',
+  'lodash',
   'service/route',
-  'service/content', 'service/qiscus'
-], function ($, _, route, $content, qiscus) {
+  'service/content',
+  'service/qiscus'
+], function($, _, route, $content, qiscus) {
   function contactFormatter(contact) {
     return `
       <li class="contact-item"
@@ -10,46 +12,42 @@ define([
         <img src="${contact.avatar_url}">
         <div class="name">${contact.username}</div>
       </li>
-    `
+    `;
   }
 
-  var searchQuery = null
-  var isLoadingUser = false
-  var isAbleToLoadMore = true
-  var totalUser = 0
+  var searchQuery = null;
+  var isLoadingUser = false;
+  var isAbleToLoadMore = true;
+  var totalUser = 0;
 
   var loadUser = _.debounce(function loadUser(currentLength) {
-    if (isLoadingUser) return
-    if (!isAbleToLoadMore) return
+    if (isLoadingUser) return;
+    if (!isAbleToLoadMore) return;
 
-    var perPage = 20
-    var currentPage = Math.ceil(currentLength / perPage)
-    var nextPage = currentPage + 1
+    var perPage = 20;
+    var currentPage = Math.ceil(currentLength / perPage);
+    var nextPage = currentPage + 1;
 
-    isLoadingUser = true
-    return qiscus.getUsers(searchQuery, nextPage)
-      .then(function (resp) {
-        isLoadingUser = false
-        var users = resp.users.map(contactFormatter).join('')
-        $(users).insertBefore('.contact-list .load-more')
+    isLoadingUser = true;
+    return qiscus.getUsers(searchQuery, nextPage).then(function(resp) {
+      isLoadingUser = false;
+      var users = resp.users.map(contactFormatter).join('');
+      $(users).insertBefore('.contact-list .load-more');
 
-        if (resp.meta.total_page === nextPage) {
-          isAbleToLoadMore = false
-          $content.find('.load-more').css({
-            display: 'none'
-          })
-        }
-      })
-
-  }, 100)
+      if (resp.meta.total_page === nextPage) {
+        isAbleToLoadMore = false;
+        $content.find('.load-more').css({
+          display: 'none'
+        });
+      }
+    });
+  }, 100);
 
   function Users() {
-    qiscus.getUsers()
-      .then(function (resp) {
-        var users = resp.users.map(contactFormatter)
-          .join('')
-        $(users).insertBefore('.contact-list .load-more')
-      })
+    qiscus.getUsers().then(function(resp) {
+      var users = resp.users.map(contactFormatter).join('');
+      $(users).insertBefore('.contact-list .load-more');
+    });
     return `
       <div class="Users">
         <div class="toolbar">
@@ -61,10 +59,7 @@ define([
         <button id="create-group-btn" class="create-group-btn">
           <i class="icon icon-new-chat-group"></i> Create Group Chat
         </button>
-        <div class="search-container">
-          <i class="icon icon-search"></i>
-          <input type="text" id="search-input" name="search-input" placeholder="Search">
-        </div>
+        
         <div class="contact-list-container">
           <div class="contact-list-title">Contacts</div>
           <ul class="contact-list">
@@ -76,52 +71,60 @@ define([
           </ul>
         </div>
       </div>
-    `
+    `;
+    /* search box depreated during back-end improvement
+    <div class="search-container">
+      <i class="icon icon-search"></i>
+      <input type="text" id="search-input" name="search-input" placeholder="Search">
+    </div>
+    */
   }
 
   $content
-    .on('click', '.Users .back-btn', function (event) {
-      event.preventDefault()
-      route.push('/chat')
+    .on('click', '.Users .back-btn', function(event) {
+      event.preventDefault();
+      route.push('/chat');
     })
-    .on('click', '.Users li.contact-item', function (event) {
-      event.preventDefault()
-      var userId = $(event.currentTarget).data('user-id')
-      qiscus.chatTarget(userId)
-        .then(function (room) {
-          route.push('/chat-room', {
-            roomName: room.name,
-            roomAvatar: room.avatar
-          })
-        })
+    .on('click', '.Users li.contact-item', function(event) {
+      event.preventDefault();
+      var userId = $(event.currentTarget).data('user-id');
+      qiscus.chatTarget(userId).then(function(room) {
+        route.push('/chat-room', {
+          roomName: room.name,
+          roomAvatar: room.avatar
+        });
+      });
     })
-    .on('input', '.Users input#search-input', _.debounce(function (event) {
-      searchQuery = event.target.value.length === 0
-        ? null
-        : event.target.value
-      if (searchQuery === null) {
-        isAbleToLoadMore = true
-        return
-      }
-      return qiscus.getUsers(searchQuery)
-        .then(function (resp) {
-          var users = resp.users.map(contactFormatter).join('')
-          $content.find('.contact-list')
+    .on(
+      'input',
+      '.Users input#search-input',
+      _.debounce(function(event) {
+        searchQuery =
+          event.target.value.length === 0 ? null : event.target.value;
+        if (searchQuery === null) {
+          isAbleToLoadMore = true;
+          return;
+        }
+        return qiscus.getUsers(searchQuery).then(function(resp) {
+          var users = resp.users.map(contactFormatter).join('');
+          $content
+            .find('.contact-list')
             .empty()
             .append(users)
-            .append('<li class="load-more"><button>Load more</button></li>')
-        })
-    }, 300))
-    .on('click', '.Users .create-group-btn', function (event) {
-      event.preventDefault()
-      route.push('/create-group')
+            .append('<li class="load-more"><button>Load more</button></li>');
+        });
+      }, 300)
+    )
+    .on('click', '.Users .create-group-btn', function(event) {
+      event.preventDefault();
+      route.push('/create-group');
     })
-    .on('click', '.Users .load-more button', function (event) {
-      event.preventDefault()
-      var childLength = $content.find('.contact-list').children().length - 1
-      loadUser(childLength)
-    })
+    .on('click', '.Users .load-more button', function(event) {
+      event.preventDefault();
+      var childLength = $content.find('.contact-list').children().length - 1;
+      loadUser(childLength);
+    });
 
-  Users.path = '/users'
-  return Users
-})
+  Users.path = '/users';
+  return Users;
+});
